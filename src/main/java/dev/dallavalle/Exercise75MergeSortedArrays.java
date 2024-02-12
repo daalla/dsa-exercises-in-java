4d259e6956a28a74d29053fd1e34ca202beaccda
Horacio Dalla Valle <horacio.dalla@despegar.com>
2024-02-12 00:00:00 -0300

    Add my solution to exercise 75

---
+++ src/main/java/dev/dallavalle/Exercise75MergeSortedArrays.java
@@ -0,0 +1,44 @@
+package dev.dallavalle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ChatGPT review of my solution: https://chat.openai.com/c/c1bf7c53-ba8e-45c0-8890-b954d4140b5b
+ * Time complexity: O(n * m)
+ * Space complexity: O(n + m)
+ */
+public class Exercise75MergeSortedArrays {
+    private int filterIndex;
+    private List<Integer> mergedArray;
+    private List<Integer> secondArrayCopy;
+
+    public List<Integer> mergeSortedArrays(List<Integer> firstArray, List<Integer> secondArray) {
+        initializeArrayCopies(firstArray, secondArray);
+        initializeFilterIndex();
+        while (!secondArrayCopy.isEmpty()) {
+            mergeSecondArray();
+        }
+        return mergedArray;
+    }
+
+    private void initializeArrayCopies(List<Integer> firstArray, List<Integer> secondArray) {
+        mergedArray = new ArrayList<>(List.copyOf(firstArray));
+        secondArrayCopy = new ArrayList<>(List.copyOf(secondArray));
+    }
+
+    private void initializeFilterIndex() {
+        filterIndex = 0;
+    }
+
+    private void mergeSecondArray() {
+        if (filterIndex >= mergedArray.size()) {
+            mergedArray.addAll(secondArrayCopy);
+        } else if (secondArrayCopy.get(0) <= mergedArray.get(filterIndex)) {
+            mergedArray.add(filterIndex, secondArrayCopy.get(0));
+            secondArrayCopy.remove(0);
+        } else {
+            filterIndex++;
+        }
+    }
+}
---
+++ src/test/java/dev/dallavalle/Exercise75MergeSortedArraysTest.java
@@ -0,0 +1,47 @@
+package dev.dallavalle;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Exercise75MergeSortedArraysTest {
+    private Exercise75MergeSortedArrays exercise75;
+
+    @BeforeEach
+    void initializeClassUnderTest() {
+        exercise75 = new Exercise75MergeSortedArrays();
+    }
+
+    @Test
+    void mergeSortedArrays_ValidArrays_ReturnsMergedSortedArray() {
+        List<Integer> firstArray = List.of(0, 3, 4, 31);
+        List<Integer> secondArray = List.of(4, 6, 30);
+        List<Integer> expectedArray = List.of(0, 3, 4, 4, 6, 30, 31);
+
+        List<Integer> returnedArray = exercise75.mergeSortedArrays(firstArray, secondArray);
+
+        assertEquals(expectedArray, returnedArray);
+    }
+
+    @Test
+    void mergeSortedArrays_EmptySecondArray_ReturnsFirstArrayValues() {
+        List<Integer> firstArray = List.of(0, 3, 4, 31);
+        List<Integer> secondArray = List.of();
+
+        List<Integer> returnedArray = exercise75.mergeSortedArrays(firstArray, secondArray);
+
+        assertEquals(firstArray, returnedArray);
+    }
+
+    @Test
+    void mergeSortedArrays_EmptyArrays_ReturnsEmptyArray() {
+        List<Integer> emptyArray = List.of();
+
+        List<Integer> returnedArray = exercise75.mergeSortedArrays(emptyArray, emptyArray);
+
+        assertEquals(emptyArray, returnedArray);
+    }
+}
